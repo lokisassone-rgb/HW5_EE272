c274f5548cab967b2611355fff58288918c834b4
lokisassone-rgb <lokisassone@gmail.com>
2026-02-11 18:41:09 -0800

    Refactor tile size calculations and loops in WeightDoubleBuffer

--- dnn-accelerator-hls-unoptimized-solution-master/src/WeightDoubleBuffer.h
+++ dnn-accelerator-hls-unoptimized-solution-master/src/WeightDoubleBuffer.h
@@ -31,27 +31,30 @@ public:
         {
             Params params = paramsIn.read();
             ac_int<ac::log2_ceil<size+1>::val, false> tileSize = params.FX * params.FY * IC0 * params.IC1;
+            ac_int<ac::log2_ceil<size+1>::val, false> tileSize_MAX = FX_MAX * FY_MAX * IC0_MAX * IC1_MAX;
+
             #pragma hls_pipeline_init_interval 1
-            TILES: for (int t = 0; t < OX1_MAX * OY1_MAX * OC1_MAX; t++) {
+            TILES: for (int t = 0; t < OX1_MAX*OY1_MAX*OC1_MAX; t++) {
                 chanStruct<PackedInt<WEIGHT_PRECISION, OC0>,size> tmp;
-                #pragma hls_pipeline_init_interval 1
-                TILE: for (int i = 0; i < tileSize; i++) {
+                TILE: for (int i = 0; i < tileSize_MAX; i++) {
                     // each packet contains 4 values, pack OC0 tgt into one row
                     PackedInt<WEIGHT_PRECISION, OC0> memRow;  // one row in the memory
-                    for (int j = 0; j < OC0; j=j+4) {
+                    for (int j = 0; j < OC0_MAX; j=j+4) {
                         PackedInt<WEIGHT_PRECISION, 4> packet = din.read();
                         #pragma hls_unroll yes
                         for (int k = 0; k < 4; k++) {
                             memRow.value[j+k] = packet.value[k];
                         }
+                        if (j == OC0_MAX-4) break;
                     }
                     tmp.data[i] = memRow;
+                    
+                    if (i == tileSize-1) break;
+
                 }  // TILE
                 dout.write(tmp);
-                
-                if(t == params.OX1 * params.OY1 * params.OC1 - 1) {
-                    break; // no more tiles to read
-                }
+
+                if (t == params.OX1 * params.OY1 * params.OC1-1) break;
             } // TILES
         }
 
@@ -87,13 +90,11 @@ public:
             TILES: for (int t = 0; t < OX1_MAX * OY1_MAX * OC1_MAX; t++) {
                 chanStruct<PackedInt<WEIGHT_PRECISION, OC0>,size> tmp;
                 tmp = din.read();
-                #pragma hls_pipeline_init_interval 1
-                TILE: for (int i = 0; i < tileSize; i++) {
+                TILE: for (int i = 0; i < FX_MAX*FY_MAX*IC0_MAX*IC1_MAX; i++) {
                     dout.write(tmp.data[i]);
+                    if (i == tileSize-1) break;
                 } // TILE
-                if (t == params.OX1 * params.OY1 * params.OC1 - 1) {
-                    break; // no more tiles to read
-                }
+                if (t == params.OX1 * params.OY1 * params.OC1 - 1) break;
             } // TILES
         }
 
